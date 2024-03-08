8bc67e8e750ef21b7019530cb64077aa340346f9
his0822 <pnu40223@gmail.com>
2024-03-08 18:00:02 +0900

    20240308

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 
 function App() {
   return (
+    <> 
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           K-digital 6기 황인상
         </p>
+        <img src={logo} className="App-logo" alt="logo" />
         
       </header>
     </div>
+    <div>
+      402호
+    </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'; // "./App" == "./App.js"
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App />  
+    {/*<App>은 내가 만든 태그, 반드시 끝에 /를 붙여서 종료해야함 */}
+
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals(console.log);
